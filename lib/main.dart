e95e343c5e41bfb0e148bb99b9245d5da51b47d2
ijull <rijal9246@gmail.com>
2024-07-15 03:02:48 +0800

    fix: fixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tb/firebase_options.dart';
 import 'package:tb/page/map_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -22,7 +28,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
@@ -30,6 +35,17 @@ class _MyHomePageState extends State<MyHomePage> {
   String _locationMessage = "";
   String longitude = "";
   String latitude = "";
+
+  late Stream<DocumentSnapshot> _locationStream;
+  final CollectionReference _locations =
+      FirebaseFirestore.instance.collection('locations');
+  final String _documentId = 'currentLocation';
+
+  @override
+  void initState() {
+    super.initState();
+    _locationStream = _locations.doc(_documentId).snapshots();
+  }
 
   void _getCurrentLocation() async {
     LocationPermission permission;
@@ -62,13 +78,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // Get the current location
     Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+      desiredAccuracy: LocationAccuracy.high,
+    );
     setState(() {
       _locationMessage =
           "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
       longitude = position.longitude.toString();
       latitude = position.latitude.toString();
     });
+
+    // Update location in Firestore
+    _updateLocation(position.latitude, position.longitude);
+  }
+
+  Future<void> _updateLocation(double latitude, double longitude) async {
+    try {
+      await _locations.doc('currentLocation').set({
+        'latitude': latitude,
+        'longitude': longitude,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Location updated successfully!'),
+      ));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Failed to update location: $e'),
+      ));
+    }
   }
 
   @override
@@ -81,6 +118,10 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text(
+              'Current Location From Local:',
+              style: TextStyle(fontSize: 24),
+            ),
             Text(
               _locationMessage,
               textAlign: TextAlign.center,
@@ -92,19 +133,63 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text("Get Current Location"),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MapPage(
-                      lat: latitude,
-                      long: longitude,
-                    ),
-                  ),
-                );
-              },
-              child: const Text("Open Map"),
+            Center(
+              child: StreamBuilder<DocumentSnapshot>(
+                stream: _locationStream,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  }
+
+                  if (!snapshot.hasData || snapshot.data!.data() == null) {
+                    return const Text('No data found');
+                  }
+
+                  final data = snapshot.data!.data() as Map<String, dynamic>;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        'Current Location From Firebase:',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Latitude: ${data['latitude']}',
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                          const SizedBox(
+                            width: 10.0,
+                          ),
+                          Text(
+                            'Longitude: ${data['longitude']}',
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MapPage(
+                                lat: data['latitude'].toString(),
+                                long: data['longitude'].toString(),
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text("Open Map"),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
           ],
         ),
